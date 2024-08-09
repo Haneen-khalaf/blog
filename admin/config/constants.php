e33ba1b6de08b681bd708b0ce2c481e0e52e6a9f
Haneen-khalaf <haneenmohammad192@gmail.com>
2024-08-09 17:01:05 +0300

    my first commit

---
+++ admin/config/constants.php
@@ -0,0 +1,9 @@
+
+
+<?php
+session_start();
+define('ROOT_URL','http://localhost/blog/');
+define('DB_HOST','localhost');
+define('DB_USER','hammad');
+define('DB_PASS', 'P@ssw0rd123');
+define('DB_NAME','blog');
